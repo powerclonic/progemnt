d52fc1322070730bc6f36bad63a8bf6ff1e08d8a
Matheus Dresch <matheus.dresch02@gmail.com>
2024-02-09 15:07:13 -0300

    Ajustes no AuthController para não utilizar o web guard

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreAuthRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
@@ -15,32 +16,18 @@ class AuthController extends Controller
      */
     public function store(StoreAuthRequest $request)
     {
-        try {
-            if (!$this->isEmail($request->identifier)) {
-                /** @var User $user */
-                $user = User::firstWhere('username', '=', $request->identifier);
 
-                if (!$user) return response("Credenciais inválidas", 401);
-            }
+        try {
+            /** @var User $user */
+            $user = User::where('username', $request->identifier)->orWhere('email', $request->identifier)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) return response("Credenciais inválidas", 401);
         } catch (\Exception $error) {
+
             Log::error('Erro logout', [$error]);
 
             return response("Um erro ocorreu durante o processo de login", 500);
         }
-
-        try {
-            $status = Auth::attempt([
-                'email' => isset($user) ? $user->email : $request->identifier,
-                'password' => $request->password
-            ], $request->remember);
-
-            if (!$status) return response("Credenciais inválidas", 401);
-        } catch (\Exception $error) {
-            return response("Um erro ocorreu durante o processo de login", 500);
-        }
-
-        /** @var User $user */
-        $user = Auth::user();
 
         return response()->json([
             'access_token' => $user->createToken('auth_token_' . now()->toISOString())->plainTextToken,
@@ -61,10 +48,5 @@ class AuthController extends Controller
             Log::error('Erro logout', [$error]);
             return response("Um erro ocorreu durante o processo de logout", 500);
         }
-    }
-
-    private function isEmail(string $value): bool
-    {
-        return preg_match("/.+\@.+\..+/", $value);
     }
 }
