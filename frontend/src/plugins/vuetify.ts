ed87895f30569f084a72a558e88c2e847d1e351e
Matheus Dresch <matheus.dresch02@gmail.com>
2024-02-15 14:37:07 -0300

    Adicionando o Vue.JS

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,37 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+const defaultTheme = {
+    dark: true,
+    colors: {
+        background: "#151419",
+        surface: "#151419",
+        primary: "#F56E0F",
+        secondary: "#878787",
+        "secondary-darken-1": "#262626",
+        "secondary-darken-2": "#1B1B1E",
+        white: "#FBFBFB",
+        warning: "#F5E10F",
+        error: "#F50F23"
+    }
+}
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+    theme: {
+        defaultTheme: "defaultTheme",
+        themes: {
+            defaultTheme
+        }
+    }
+})
